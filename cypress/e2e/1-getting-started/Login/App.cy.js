e7854b4c64a40afe1f38d63d73ca0fae375dae74
rodrigo freire <rodrigo.freire@totvs.com.br>
2024-01-05 10:38:21 -0300

    projeto inicial

---
+++ cypress/e2e/1-getting-started/Login/App.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress"/>
+
+
+describe('Teste Funcional de Login', () => {
+    it('Deve realizar o login com sucesso', () => {
+        cy.visit("https://www.saucedemo.com/v1/")
+        
+    });
+});
